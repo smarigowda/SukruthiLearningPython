9404d729120ec79002f5a304786bdaaae87f08b0
Marigowda, Santosh <santosharakere@gmail.com>
2016-12-18 17:09:48 +0000

    cleaning number

--- Week__4.py
+++ Week__4.py
@@ -26,19 +26,27 @@
 
 #     print("Buy " + item)
 
-meal = ["egg", "bacon", "tomato", "rice"]
+# meal = ["egg", "bacon", "tomato", "rice"]
 
-for item in meal:
-    if item == 'bacon':
-        nasty_food_item = item
-        break
-#
-if nasty_food_item:
-# #instead of underscore can put nastyFoodItem
-    print("Can i have anything without bacon in it?")
+# for item in meal:
+#     if item == 'bacon':
+#         nasty_food_item = item
+#         break
+# #
+# if nasty_food_item:
+# # #instead of underscore can put nastyFoodItem
+#     print("Can i have anything without bacon in it?")
 
 
+number = "9,223,272,036,854,775,807"
+cleanedNumber = ''
 
+for i in range(0, len(number)):
+    if number[i] in '0123456789':
+        cleanedNumber = cleanedNumber + number[i]
+
+newNumber = int(cleanedNumber)
+print("The number is {} ".format(newNumber))
 
 
 
