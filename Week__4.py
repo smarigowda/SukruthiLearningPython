35b3aecb0e00ec5e81a8bb82ad1f63421fda559d
Marigowda, Santosh <santosharakere@gmail.com>
2016-12-18 16:50:26 +0000

    for loop

--- Week__4.py
+++ Week__4.py
@@ -1,9 +1,12 @@
-number = "9,223,372,036,854,775,807"
-cleanedNumber = ''
+# number = "9,223,372,036,854,775,807"
+# cleanedNumber = ''
 
-for char in number:
-    if char in '0123456789':
-        cleanedNumber = cleanedNumber + char
-#
-newNumber = int(cleanedNumber)
-print("The number is {}".format(newNumber))
+# for char in number:
+#     if char in '0123456789':
+#         cleanedNumber = cleanedNumber + char
+# #
+# newNumber = int(cleanedNumber)
+# print("The number is {}".format(newNumber))
+
+for state in [" not pinin'", " no more", " a stiff", " bereft of life"]:
+    print("This parrot is"+ state)
