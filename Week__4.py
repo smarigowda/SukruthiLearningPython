fc43835f88fb54bd0fd0887be2abe64f2d512ea1
Marigowda, Santosh <santosharakere@gmail.com>
2016-12-18 17:05:02 +0000

    using break in for

--- Week__4.py
+++ Week__4.py
@@ -14,11 +14,17 @@
 # for i in range(0, 100, 5):
 #     print("i is {} ".format(i))
 
-for i in range(1,13):
-    for j in range(1,13):
-        print("{1} times {0} is {2}". format(i, j, i*j))
-        print("=============")
+# for i in range(1,13):
+#     for j in range(1,13):
+#         print("{1} times {0} is {2}". format(i, j, i*j))
+#         print("=============")
 
+shopping_list = ["milk", "pasta", "eggs", "spam", "bread", "rice"]
+for item in shopping_list:
+    if item == 'spam':
+        break
+
+    print("Buy " + item)
 
 
 
