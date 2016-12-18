f29c5a15653fa0e4053dec1dcbc7079f10f3fb28
Marigowda, Santosh <santosharakere@gmail.com>
2016-12-18 17:11:06 +0000

    common operations on numbers

--- Week__4.py
+++ Week__4.py
@@ -38,22 +38,31 @@
 #     print("Can i have anything without bacon in it?")
 
 
-number = "9,223,272,036,854,775,807"
-cleanedNumber = ''
+# number = "9,223,272,036,854,775,807"
+# cleanedNumber = ''
 
-for i in range(0, len(number)):
-    if number[i] in '0123456789':
-        cleanedNumber = cleanedNumber + number[i]
+# for i in range(0, len(number)):
+#     if number[i] in '0123456789':
+#         cleanedNumber = cleanedNumber + number[i]
 
-newNumber = int(cleanedNumber)
-print("The number is {} ".format(newNumber))
+# newNumber = int(cleanedNumber)
+# print("The number is {} ".format(newNumber))
 
+x = 23
+x += 1
+print(x)
 
+x -= 4
+print(x)
 
+x *= 5
+print(x)
 
+x /= 4
+print(x)
 
+x **=2
+print(x)
 
-
-
-
-
+x %= 60
+print(x)
