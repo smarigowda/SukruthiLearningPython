2b0c69276fd81be5b9aa197b33d8e1be9cfc9f1f
Marigowda, Santosh <santosharakere@gmail.com>
2016-12-18 17:03:48 +0000

    nested for loop

--- Week__4.py
+++ Week__4.py
@@ -11,10 +11,13 @@
 # for state in [" not pinin'", " no more", " a stiff", " bereft of life"]:
 #     print("This parrot is"+ state)
 
-for i in range(0, 100, 5):
-    print("i is {} ".format(i))
+# for i in range(0, 100, 5):
+#     print("i is {} ".format(i))
 
-
+for i in range(1,13):
+    for j in range(1,13):
+        print("{1} times {0} is {2}". format(i, j, i*j))
+        print("=============")
 
 
 
