a111b7452069b2b688441911c4e7e1825b3eead5
Marigowda, Santosh <santosharakere@gmail.com>
2016-12-26 16:28:11 +0000

    parsing IP address

--- Week__5.py
+++ Week__5.py
@@ -1,6 +1,24 @@
-greeting = " Good "
-greeting += "morning"
-print (greeting)
-#
-greeting *= 5
-print(greeting)
+# greeting = " Good "
+# greeting += "morning"
+# print (greeting)
+# #
+# greeting *= 5
+# print(greeting)
+
+input_prompt = "Please enter your IP address. An Ip address consists os 4 numbers " \
+				"separated from each other by a full stop. "
+ipAddress = input(input_prompt)
+if ipAddress[-1] != '.':
+ ipAddress += '.'
+
+segment = 1
+segment_length = 0
+character = ""
+
+for character in ipAddress:
+	if character == '.':
+		print("segment {} contains {} characters".format(segment, segment_length))
+		segment += 1
+		segment_length = 0
+	else:
+		segment_length += 1
