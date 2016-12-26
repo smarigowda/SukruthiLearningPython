a40ebdec82a27ec16cdc0137df275f87deabf424
Marigowda, Santosh <santosharakere@gmail.com>
2016-12-26 16:30:37 +0000

    for and while loop

--- Week__5.py
+++ Week__5.py
@@ -5,20 +5,28 @@
 # greeting *= 5
 # print(greeting)
 
-input_prompt = "Please enter your IP address. An Ip address consists os 4 numbers " \
-				"separated from each other by a full stop. "
-ipAddress = input(input_prompt)
-if ipAddress[-1] != '.':
- ipAddress += '.'
+# input_prompt = "Please enter your IP address. An Ip address consists os 4 numbers " \
+# 				"separated from each other by a full stop. "
+# ipAddress = input(input_prompt)
+# if ipAddress[-1] != '.':
+#  ipAddress += '.'
 
-segment = 1
-segment_length = 0
-character = ""
+# segment = 1
+# segment_length = 0
+# character = ""
 
-for character in ipAddress:
-	if character == '.':
-		print("segment {} contains {} characters".format(segment, segment_length))
-		segment += 1
-		segment_length = 0
-	else:
-		segment_length += 1
+# for character in ipAddress:
+# 	if character == '.':
+# 		print("segment {} contains {} characters".format(segment, segment_length))
+# 		segment += 1
+# 		segment_length = 0
+# 	else:
+# 		segment_length += 1
+
+for i in range(11):
+	print("i is now {}".format(i))
+
+i = 0
+while i < 10:
+	print("i is now {}".format(i))
+	i += 1
