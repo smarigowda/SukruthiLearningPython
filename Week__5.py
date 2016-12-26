8264c015350ac4d9413a7e4805aadf64d487a11b
Marigowda, Santosh <santosharakere@gmail.com>
2016-12-26 16:36:27 +0000

    random number, number guessing game

--- Week__5.py
+++ Week__5.py
@@ -31,16 +31,35 @@
 # 	print("i is now {}".format(i))
 # 	i += 1
 
-available_exits = ["east", "north east", "south"]
+# available_exits = ["east", "north east", "south"]
 
-chosen_exit = ""
-while chosen_exit not in available_exits:
-	chosen_exit = input("Please choose a direction")
-	if chosen_exit == "quit":
-		print("Game over")
-		break
+# chosen_exit = ""
+# while chosen_exit not in available_exits:
+# 	chosen_exit = input("Please choose a direction")
+# 	if chosen_exit == "quit":
+# 		print("Game over")
+# 		break
 
-print("aren't you glad you got out of there")
+# print("aren't you glad you got out of there")
 
 
+import random
+
+highest = 10
+answer = random.randint(1, highest)
+
+print("Please guess a number between 1 and {}".format(highest))
+guess = int(input())
+if guess != answer:
+	if guess < answer:
+		print("Please guess higher")
+	else: # guess must be higher than number
+		print("Please guess lower")
+	guess = int(input())
+	if guess == answer:
+		print("Well done you guessed it")
+	else:
+		print("Sorry you have not guessed correctly")
+else:
+	print("You got it first time, well done!")
 
