b69489afc2947153f59bcbeda3e36602cc1c383d
Marigowda, Santosh <santosharakere@gmail.com>
2017-01-29 12:15:42 +0000

    comparing 2 arrays

--- Week__6.1.py
+++ Week__6.1.py
@@ -6,9 +6,27 @@
 
 # print(ipAddress.count("."))
 
-colour_list = ["red", "green", "blue", "yellow", "purple"]
+# colour_list = ["red", "green", "blue", "yellow", "purple"]
 
-colour_list.append("black")
+# colour_list.append("black")
 
-for state in colour_list:
-	print("The colour is " + state)
+# for state in colour_list:
+# 	print("The colour is " + state)
+
+even = [2, 4, 6, 8]
+odd = [1, 3, 5, 7, 9]
+
+numbers = even + odd
+numbers_in_order = sorted(numbers)
+
+print(numbers_in_order)
+
+if numbers == numbers_in_order:
+	print("The lists are equal")
+else:
+	print("The lists are not equal")
+
+if numbers_in_order == sorted(numbers):
+	print("The lists are equal")
+else:
+	print("The lists are not equal")
