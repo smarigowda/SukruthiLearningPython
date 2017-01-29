a15b089f1066c724c95eab9233b161fe9528f7a2
Marigowda, Santosh <santosharakere@gmail.com>
2017-01-29 11:53:14 +0000

    same program but using loop

--- Week__6.py
+++ Week__6.py
@@ -1,41 +1,18 @@
-# small number game
+# big number game
 import random
 
-highest = 10
+highest = 1000
 answer = random.randint(1, highest)
 
-print("Please guess a number between 1 and {}".format(highest))
-
-guess = int(input()) # first attempt
-
-if guess != answer:
+print("Please guess a number between 1 and {}: ".format(highest))
+guess = 0
+while guess != answer:
+	guess = int(input())
+	if guess == 0:
+		break
 	if guess < answer:
-		print("Please guess higher")
+		print("Please guess higher.")
+	elif guess > answer:
+		print("Please guess lower.")
 	else:
-		print("Please guess lower")
-
-	guess = int(input()) # second attempt
-	if guess == answer:
-		print("Well done you guessed it 2nd time")
-		exit(0)
-	elif guess < answer:
-		print("Please guess higher")
-	else:
-		print("Please guess lower")
-
-	guess = int(input()) # third attempt
-	if guess == answer:
-		print("Well done you got it!!")
-		exit(0)
-	elif guess < answer:
-		print("Please guess higher")
-	else:
-		print("Please guess lower")
-
-	guess = int(input()) # fourth attempt
-	if guess == answer:
-		print("You finally got the answer!")
-	else:
-		print("Sorry you failed to guess correctly.")
-else:
-	print("You got it first time, well done!")
+		print("Well done, you guessed it")
