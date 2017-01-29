0f8971689a36265b364db178b1c85266dc48354a
Marigowda, Santosh <santosharakere@gmail.com>
2017-01-29 12:27:02 +0000

    2d array

--- Week__6.1.py
+++ Week__6.1.py
@@ -45,10 +45,26 @@
 
 # print(list("Das list ist equal"))
 
+# even = [2, 4, 6, 8]
+# another_even = sorted(even, reverse=True)
+
+# print(another_even == even)
+
+# another_even.sort(reverse=True)
+# print(even)
+
 even = [2, 4, 6, 8]
-another_even = sorted(even, reverse=True)
+odd = [1, 3, 5, 7]
 
-print(another_even == even)
+numbers = [even, odd]
 
-another_even.sort(reverse=True)
-print(even)
+for number_set in numbers:
+	print(number_set)
+
+	for value in number_set:
+		print(value)
+
+
+
+
+
