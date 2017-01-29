a3396da16bf5e9532e0dfa7d33b46c1493bd4b98
Marigowda, Santosh <santosharakere@gmail.com>
2017-01-29 12:12:13 +0000

    Array example

--- Week__6.1.py
+++ Week__6.1.py
@@ -1,7 +1,14 @@
-import sys
+# import sys
 
-print("Please enter an ipAddress in the format xxx.yyyy.zzzz.nnnn: ")
+# print("Please enter an ipAddress in the format xxx.yyyy.zzzz.nnnn: ")
 
-ipAddress = sys.stdin.readline()
+# ipAddress = sys.stdin.readline()
 
-print(ipAddress.count("."))
+# print(ipAddress.count("."))
+
+colour_list = ["red", "green", "blue", "yellow", "purple"]
+
+colour_list.append("black")
+
+for state in colour_list:
+	print("The colour is " + state)
