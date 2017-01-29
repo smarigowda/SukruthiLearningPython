24e9ffe7ce4a1ccc120afc54d87676c2d9c2ff8e
Marigowda, Santosh <santosharakere@gmail.com>
2017-01-29 12:23:42 +0000

    2 ways of sorting arrays

--- Week__6.1.py
+++ Week__6.1.py
@@ -31,16 +31,24 @@
 # else:
 # 	print("The lists are not equal")
 
-list_1 = []
-list_2 = list()
-#both lists are empty and come out as the same
+# list_1 = []
+# list_2 = list()
+# #both lists are empty and come out as the same
 
-print("list 1: {}".format(list_1))
-print("list 2: {}".format(list_2))
+# print("list 1: {}".format(list_1))
+# print("list 2: {}".format(list_2))
 
-if list_1 == list_2:
-	print("Das list ist equal.")
-else:
-	print("Das list ist nicht equal.")
+# if list_1 == list_2:
+# 	print("Das list ist equal.")
+# else:
+# 	print("Das list ist nicht equal.")
 
-print(list("Das list ist equal"))
+# print(list("Das list ist equal"))
+
+even = [2, 4, 6, 8]
+another_even = sorted(even, reverse=True)
+
+print(another_even == even)
+
+another_even.sort(reverse=True)
+print(even)
