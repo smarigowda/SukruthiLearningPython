e9da06ce7b29c44d0dc9a6ac7388ab8bb065b6d1
Marigowda, Santosh <santosharakere@gmail.com>
2017-01-29 12:20:02 +0000

    using list function

--- Week__6.1.py
+++ Week__6.1.py
@@ -13,20 +13,34 @@
 # for state in colour_list:
 # 	print("The colour is " + state)
 
-even = [2, 4, 6, 8]
-odd = [1, 3, 5, 7, 9]
+# even = [2, 4, 6, 8]
+# odd = [1, 3, 5, 7, 9]
 
-numbers = even + odd
-numbers_in_order = sorted(numbers)
+# numbers = even + odd
+# numbers_in_order = sorted(numbers)
 
-print(numbers_in_order)
+# print(numbers_in_order)
 
-if numbers == numbers_in_order:
-	print("The lists are equal")
+# if numbers == numbers_in_order:
+# 	print("The lists are equal")
+# else:
+# 	print("The lists are not equal")
+
+# if numbers_in_order == sorted(numbers):
+# 	print("The lists are equal")
+# else:
+# 	print("The lists are not equal")
+
+list_1 = []
+list_2 = list()
+#both lists are empty and come out as the same
+
+print("list 1: {}".format(list_1))
+print("list 2: {}".format(list_2))
+
+if list_1 == list_2:
+	print("Das list ist equal.")
 else:
-	print("The lists are not equal")
+	print("Das list ist nicht equal.")
 
-if numbers_in_order == sorted(numbers):
-	print("The lists are equal")
-else:
-	print("The lists are not equal")
+print(list("Das list ist equal"))
