dcbb5a8ac53e622c22b00a66e1df20481f71f87e
Marigowda, Santosh <santosharakere@gmail.com>
2017-02-12 10:23:06 +0000

    creating list out of string

--- Week__7.py
+++ Week__7.py
@@ -1,13 +1,15 @@
-list_1 = []
-list_2 = list()
-list_3 = ["non empty list"]
+# list_1 = []
+# list_2 = list()
+# list_3 = ["non empty list"]
 
-print("List 1: {}".format(list_1))
-print("List 2: {}".format(list_2))
-if list_1 == list_2:
-	print("The lists are equal") # empty lists are equal
+# print("List 1: {}".format(list_1))
+# print("List 2: {}".format(list_2))
+# if list_1 == list_2:
+# 	print("The lists are equal") # empty lists are equal
 
-if list_1 == list_3:
-	print("list 1 and 3 are equal")
-else:
-	print("list 1 and 3 are not equal")
+# if list_1 == list_3:
+# 	print("list 1 and 3 are equal")
+# else:
+# 	print("list 1 and 3 are not equal")
+
+print(list("The lists are equal"))
