08f042d3400c581bef1dbb135cc48e5e09c8feb6
Marigowda, Santosh <santosharakere@gmail.com>
2017-02-12 17:44:21 +0000

    automatic iteration

--- Week__7.py
+++ Week__7.py
@@ -34,17 +34,25 @@
 # 		for ingredient in meal:
 # 			print(ingredient)
 
+# string = "1234567890"
+
+# my_iterator = iter(string)
+# print(my_iterator)
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+# print(next(my_iterator))
+
 string = "1234567890"
 
-my_iterator = iter(string)
-print(my_iterator)
-print(next(my_iterator))
-print(next(my_iterator))
-print(next(my_iterator))
-print(next(my_iterator))
-print(next(my_iterator))
-print(next(my_iterator))
-print(next(my_iterator))
-print(next(my_iterator))
-print(next(my_iterator))
-print(next(my_iterator))
+for char in string:
+	print(char)
+
+for char in iter(string): # this is what the computer does
+	print(char)
