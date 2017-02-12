c4749747ccf55ae7f711dd8f000e7738360d0b51
Marigowda, Santosh <santosharakere@gmail.com>
2017-02-12 18:07:42 +0000

    counting in sevens

--- Week__7.py
+++ Week__7.py
@@ -68,35 +68,38 @@
 # 	next_item = next(my_iterator)
 # 	print(next_item)
 
-my_list = list(range(10))
-print(my_list)
-#
-even = list(range(0, 10, 2))
-odd = list(range(1, 10, 2))
-#
-print(even)
-print(odd)
+# my_list = list(range(10))
+# print(my_list)
+# #
+# even = list(range(0, 10, 2))
+# odd = list(range(1, 10, 2))
+# #
+# print(even)
+# print(odd)
 
-##################################
+# ##################################
 
-my_string ="abcdefghijklmnopqrstuvwxyz"
-print(my_string.index('e'))
-print(my_string[4])
+# my_string ="abcdefghijklmnopqrstuvwxyz"
+# print(my_string.index('e'))
+# print(my_string[4])
 
-###################################
+# ###################################
 
-small_decimals = range(0, 10)
-print(small_decimals)
+# small_decimals = range(0, 10)
+# print(small_decimals)
 
-print(small_decimals.index(3))
+# print(small_decimals.index(3))
 
-##################################
+# ##################################
 
-odd = range(1, 10000, 2)
-print(odd)
+# odd = range(1, 10000, 2)
+# print(odd)
 
-print(odd[985]) # It gives the 985th odd number
+# print(odd[985]) # It gives the 985th odd number
 
-
+sevens = range(7, 1000000, 7)
+x = int(input("Please enter a positive number less than 1 million,if divisible by 7 an answer is given: "))
+if x in sevens:
+	print("{} is divisible by seven". format(x))
 
 
